b6cd7f64e9e3464c2fab584cdfe07f2c0749cb37
AvniB82 <bicmeti.avni@gmail.com>
2024-02-14 22:05:40 +0000

    na

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
-import { DataContext } from './components/DataContext.jsx';
+import { DataContext } from './components/DataContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
