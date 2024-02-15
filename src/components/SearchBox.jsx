2a61c70d6129366921330e2962766d34d5cb896d
AvniB82 <bicmeti.avni@gmail.com>
2024-02-15 17:15:29 +0000

    added hero banner, added logo, embeded the searchbox inthe hero. added error message (with Michael) to show when a user searches for something outside the peramiters, added css to everything mentioned above

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,31 +1,55 @@
 import React, { useState } from 'react';
 import { DataCtxt, useContext } from './DataContext';
+import './SearchBox.css';
+
 
 export default function SearchBox() {
   const { fetchSymbols } = useContext(DataCtxt);
   const [searchTerm, setSearchTerm] = useState('');
+  const [error, setError] = useState('');
 
   const handleInputChange = (event) => {
     const inputValue = event.target.value;
     setSearchTerm(inputValue);
   };
 
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-    fetchSymbols(searchTerm);
+
+    try {
+      
+      const response = await fetchSymbols(searchTerm);
+
+      if (response.data.bestMatches.length === 0) {
+        setError('No matching stocks found. Please enter a valid symbol.');
+      } else {
+        setError('');
+      }
+    } catch (error) {
+      setError(
+        'There are no matching stocks or free API limit has been reached'
+      );
+    }
   };
 
   return (
-    <div>
-      <form className='form' onSubmit={handleFormSubmit}>
-        <input
-          type='text'
-          placeholder='Search by symbol or name'
-          value={searchTerm}
-          onChange={handleInputChange}
-        />
-        <button type='submit'>Search</button>
-      </form>
+    <div className='hero-banner'>
+      <img src="src/images/Logo.png" alt="Logo" className="logo" />
+      <div className="search-container">
+        <form className='form' onSubmit={handleFormSubmit}>
+          <input
+            type='text'
+            placeholder='Search by symbol or name'
+            value={searchTerm}
+            onChange={handleInputChange}
+            className='input-field'
+          />
+          <button type='submit' className="button">Search</button>
+        </form>
+        {error && (
+          <div className="error-message" style={{ color: 'white', textAlign: 'center' }}>{error}</div>
+        )}
+      </div>
     </div>
   );
-}
+        }
